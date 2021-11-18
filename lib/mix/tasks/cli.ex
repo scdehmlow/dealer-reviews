009623bf5c35d876319a1135ae629573aebbe323
Shaffer <scdehmlow@gmail.com>
2021-11-17 20:53:38 -0700

    Add mix task to run application

---
+++ lib/mix/tasks/cli.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Cli do
+  @moduledoc "CLI task: mix help cli"
+  use Mix.Task
+
+  @shortdoc "Runs the CLI main method to print to console."
+  def run(args) do
+    DealerReviews.CLI.main(args)
+  end
+end
